eca4471c17f6188b0033f25e0beeb704b65f4a27
Igor <igormetring@gmail.com>
2025-11-16 17:36:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,120 @@
-let janela = document.querySelector('.janela');
-let overlay = document.querySelector('.overlay');
-let btnFechar = document.querySelector('.close-modal');
-let cards = document.querySelectorAll('.caixa');
-let areas = document.querySelectorAll('.card');
+// tema escuro
+let body = document.body;
+let bntTema = document.querySelector('.logo-darkmode');
 
-const removeHidden = function () {
-    janela.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+bntTema.addEventListener('click', () => {
+  body.classList.toggle('darkmode');
+
+  if (body.classList.contains('darkmode')) {
+    bntTema.textContent = '🌙';
+  } else {
+    bntTema.textContent = '🌞';
+  }
+});
+// tema escuro
+
+//modal window
+let modal1 = document.querySelector('.dialog-1');
+let modal2 = document.querySelector('.dialog-2');
+let modal3 = document.querySelector('.dialog-3');
+let modal4 = document.querySelector('.dialog-4');
+let modal5 = document.querySelector('.dialog-5');
+let modal6 = document.querySelector('.dialog-6');
+
+let card1 = document.querySelector('#glass-1');
+let card2 = document.querySelector('#glass-2');
+let card3 = document.querySelector('#glass-3');
+let card4 = document.querySelector('#card-1');
+let card5 = document.querySelector('#card-2');
+let card6 = document.querySelector('#card-3');
+
+let btnFechar1 = modal1.querySelector('.close-modal');
+let btnFechar2 = modal2.querySelector('.close-modal');
+let btnFechar3 = modal3.querySelector('.close-modal');
+let btnFechar4 = modal4.querySelector('.close-modal');
+let btnFechar5 = modal5.querySelector('.close-modal');
+let btnFechar6 = modal6.querySelector('.close-modal');
+
+card1.onclick = function () {
+  modal1.showModal();
 }
 
-const addHidden = function () {
-  janela.classList.add('hidden');
-  overlay.classList.add('hidden');
+card2.onclick = function () {
+  modal2.showModal();
+}
+
+card3.onclick = function () {
+  modal3.showModal();
+}
+
+card4.onclick = function () {
+  modal4.showModal();
+}
+
+card5.onclick = function () {
+  modal5.showModal();
+}
+
+card6.onclick = function () {
+  modal6.showModal();
+}
+
+btnFechar1.onclick = function () {
+  modal1.close();
 };
 
-for (let i = 0; i < areas.length; i++) {
-    areas[i].addEventListener('click', removeHidden, function () {
-    })
-    };
+btnFechar2.onclick = function () {
+  modal2.close();
+};
 
-for (let i = 0; i < cards.length; i++) {
-  cards[i].addEventListener('click', removeHidden)
-}
+btnFechar3.onclick = function () {
+  modal3.close();
+};
 
+btnFechar4.onclick = function () {
+  modal4.close();
+};
 
+btnFechar5.onclick = function () {
+  modal5.close();
+};
 
-overlay.addEventListener('click', addHidden);
-btnFechar.addEventListener('click', addHidden);
+btnFechar6.onclick = function () {
+  modal6.close();
+};
+
+modal1.onclick = function (e) {
+  if (e.target === modal1) {
+    modal1.close();
+  }
+};
+
+modal2.onclick = function (e) {
+  if (e.target === modal2) {
+    modal2.close();
+  }
+};
+
+modal3.onclick = function (e) {
+  if (e.target === modal3) {
+    modal3.close();
+  }
+};
+
+modal4.onclick = function (e) {
+  if (e.target === modal4) {
+    modal4.close();
+  }
+};
+
+modal5.onclick = function (e) {
+  if (e.target === modal5) {
+    modal5.close();
+  }
+};
+
+modal6.onclick = function (e) {
+  if (e.target === modal6) {
+    modal6.close();
+  }
+};
